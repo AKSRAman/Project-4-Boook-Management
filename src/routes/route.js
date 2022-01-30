270853fef2b86158c3128f34ac56217139e01ad6
Aman Kumar <you@example.com>
2022-01-30 18:27:49 +0530

    Project-4-Final

---
+++ src/routes/route.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController')
+const bookController = require('../controllers/bookController')
+const reviewController = require('../controllers/reviewController')
+const myMiddleware = require('../middleWares/middleWare')
+const AWSController = require('../controllers/AWSController')
+
+router.post('/register', userController.userRegistration)
+router.post('/login', userController.userLogin)
+router.post('/books', myMiddleware.getUserDetails, bookController.createBook)
+router.get('/books', myMiddleware.getUserDetails, bookController.getQueryBooks)
+router.get('/books/:bookId', myMiddleware.getUserDetails, bookController.getParamsBook)
+router.put('/books/:bookId', myMiddleware.getUserDetails, bookController.updateBookById)
+router.delete('/books/:bookId', myMiddleware.getUserDetails, bookController.deleteBookById)
+router.post('/books/:bookId/review', reviewController.createReview)
+router.put('/books/:bookId/review/:reviewId', reviewController.updateReview)
+router.delete('/books/:bookId/review/:reviewId', reviewController.deleteReview)
+router.post("/write-file-aws", AWSController.myAws )
+
+//---------------------------selfTest----------------------------//
+router.get('/testCount', bookController.testBoookCount)
+router.get('/testAPI', bookController.testAPI)
+
+module.exports = router;
